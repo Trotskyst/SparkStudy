956e1a71372806211d4539f31d1ee754576dfa37
Trotskyst <trotsky@mail.ru>
2022-06-03 10:40:38 +0300

    Доработка по RDD после замечаний

--- optim_6_5_2_RDD.scala
+++ optim_6_5_2_RDD.scala
@@ -1,9 +1,26 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
-object optim_6_5_2_RDD extends App {
+// Параметры загрузки
+object LoadParameter {
+  val fileNameTransactions: String = "src/main/resources/transactions.csv"
+  val fileNameCountries: String = "src/main/resources/country_codes.csv"
+}
+
+// Параметры выгрузки
+object SaveParameter {
+  val fileNameResult: String = "src/main/resources/result.parquet"
+}
+
+// Условия задания
+object TaskCondition {
+  val countryName = "United Kingdom"
+  val date = "2018-12-01"
+}
+
+object optim_6_5_3_RDD extends App {
   Logger.getLogger("org").setLevel(Level.ERROR)
 
   val spark = SparkSession.builder()
@@ -13,31 +30,97 @@ object optim_6_5_2_RDD extends App {
 
   import spark.implicits._
 
-  // Загрузка файла
-  val loadCsv = (sparkContext: SparkContext, fileName: String) =>
+  case class Country (
+    name: String,
+    code: String
+  )
+
+  case class Transaction (
+    transactionNo: String,
+    date: String,
+    productName: String,
+    countryCode: String
+  )
+
+  case class Result (
+    countryCode: String,
+    countryName: String,
+    date: String,
+    mostPopularProduct: String,
+    avgTransaction: BigDecimal
+  )
+
+  // Загрузка страны
+  val loadCsvCountry = (sparkContext: SparkContext, fileName: String, taskConditionCountryName: String) =>
     sparkContext.textFile(fileName)
-    .map(line => line.split(","))
+      .map { line =>
+        val fields = line.split(",")
+        Country(fields(0), fields(1))
+      }
+      .filter(country => country.name == taskConditionCountryName)
+      .first()
 
-  // Находим код страны
-  def getCountryCode(
-    rdd: RDD[Array[String]],
-    taskConditionCountryName: String)
-  : String = {
-    // Находим информацию об искомой стране
-    val currentCountry = rdd
-      .filter(x => x(0) == taskConditionCountryName)
-      .first
+  // Условие загрузки транзакций
+  val loadCsvTransactionCondition = (
+    value: Transaction,
+    conditionDate: String,
+    conditionCountry: String
+  ) =>
+    value.date != null &&
+    value.countryCode != null &&
+    value.date == conditionDate &&
+    value.countryCode == conditionCountry
 
-    currentCountry(1)
+  // Загрузка транзакций
+  val loadCsvTransaction = (
+    sparkContext: SparkContext,
+    fileName: String,
+    conditionDate: String,
+    conditionCountry: String
+  ) =>
+    sparkContext.textFile(fileName)
+      .map { line =>
+        val fields = line.split(",")
+        Transaction(fields(0), fields(1), fields(3), fields(7))
+      }
+      .filter(value => loadCsvTransactionCondition(value, conditionDate, conditionCountry))
+
+  // Сохранение результата
+  def saveResult (
+    fileName: String,
+    countryCode: String,
+    countryName: String,
+    date: String,
+    mostPopularProduct: String,
+    avgTransaction: BigDecimal
+  ): Unit = {
+    val result = Seq(
+      Result(
+        countryCode,
+        countryName,
+        date,
+        mostPopularProduct,
+        avgTransaction
+      )
+    )
+    result
+      .toDF(
+        "countryCode",
+        "countryName",
+        "date",
+        "mostPopularProduct",
+        "avgTransaction")
+      .write
+      .mode(SaveMode.Overwrite)
+      .save(fileName)
   }
 
   // Получить вектор из элементов колонки RDD
-  def getColumn(rdd: RDD[Array[String]], columnNumber: Int): Vector[String] = {
-    val columnRDD = rdd.map(x => Column(x(columnNumber)))
-    val columnDF = spark.createDataFrame(columnRDD)
-
-    columnDF
-      .map(x => x.getAs[String](0))
+  def getColumn(rdd: RDD[Transaction], columnName: String): Vector[String] = {
+    rdd.map(line => columnName match {
+        case "products" => line.productName
+        case "transactions" => line.transactionNo
+      })
       .collect
       .toVector
   }
@@ -69,40 +152,22 @@ object optim_6_5_2_RDD extends App {
       .setScale(scale, BigDecimal.RoundingMode.HALF_UP)
   }
 
-  // Параметры загрузки
-  object LoadParameter {
-    val FileNameTransactions: String = "src/main/resources/transactions.csv"
-    val FileNameCountries: String = "src/main/resources/country_codes.csv"
-  }
-
-  // Условия задания
-  object TaskCondition {
-    val CountryName = "United Kingdom"
-    val Date = "2018-12-01"
-  }
-
-  case class Column (
-    Value: String
-  )
-
   val sc = spark.sparkContext
 
-  // Страны
-  val countriesRDD = loadCsv(sc, LoadParameter.FileNameCountries)
-
-  // Код страны
-  val currentCountryCode = getCountryCode(
-    countriesRDD,
-    TaskCondition.CountryName)
+  // Страна
+  val country = loadCsvCountry(sc, LoadParameter.fileNameCountries, TaskCondition.countryName)
 
   // Транзакции
-  val transactionsRDD = loadCsv(sc, LoadParameter.FileNameTransactions)
-    .filter(x => x(1) == TaskCondition.Date && x(7) == currentCountryCode)
+  val transactionsRDD = loadCsvTransaction(
+    sc,
+    LoadParameter.fileNameTransactions,
+    TaskCondition.date,
+    sc.broadcast(country).value.code)
 
   // Вектор с продуктами
-  val products = getColumn(transactionsRDD, 3)
+  val products = getColumn(transactionsRDD, "products")
   // Вектор с транзакциями
-  val transactions = getColumn(transactionsRDD, 0)
+  val transactions = getColumn(transactionsRDD, "transactions")
 
   // Наиболее часто покупаемый товар
   val mostPopularProduct = getMostPopularProduct(products)
@@ -111,8 +176,18 @@ object optim_6_5_2_RDD extends App {
   val avgTransaction =  getAvgTransaction(
     transactions,
     1)
+  
   println(s"Наиболее часто покупаемый товар - $mostPopularProduct")
   println(s"Сколько товаров в среднем посетители приобретают за одну транзакцию - $avgTransaction")
+
+  // Сохранение результата в файл
+  saveResult(
+    SaveParameter.fileNameResult,
+    country.code,
+    TaskCondition.countryName,
+    TaskCondition.date,
+    mostPopularProduct,
+    avgTransaction)
 
 //  System.in.read()
 
